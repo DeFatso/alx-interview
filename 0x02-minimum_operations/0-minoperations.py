b61f40835ff1545b6006cc7ff881692f346d2d84
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-05-09 21:13:05 +0000

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+
+def minOperations(n):
+    """Returns an integer"""
+
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while factor * factor <= n:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    if n > 1:
+        operations += n
+
+    return operations
